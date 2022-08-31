e845776e578477f33e7883bfa41704521ac7362e
Lukasz D <lukasz.dacz@llinformatics.com>
2022-08-31 10:47:17 +0200

    init commit

---
+++ lib/checkeyatex.ex
@@ -0,0 +1,18 @@
+defmodule Checkeyatex do
+  @moduledoc """
+  Documentation for `Checkeyatex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Checkeyatex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/checkeyatex_test.exs
@@ -0,0 +1,8 @@
+defmodule CheckeyatexTest do
+  use ExUnit.Case
+  doctest Checkeyatex
+
+  test "greets the world" do
+    assert Checkeyatex.hello() == :world
+  end
+end
