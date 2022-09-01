65a42e5a3fdaad61297fab48884633b1bd86c946
Lukasz D <lukasz.dacz@llinformatics.com>
2022-09-01 10:00:16 +0200

    write simple module

--- lib/checkeyatex.ex
+++ lib/checkeyatex.ex
@@ -12,7 +12,34 @@ defmodule Checkeyatex do
       :world
 
   """
-  def hello do
-    :world
+
+  def start(_, _) do
+    start_link()
+  end
+  @yata_key :yatapp_translations
+  @name :yata_key_checker
+  def start_link() do
+    initial_value =
+      :ets.match(:yatapp_translations, :"$1")
+      |> List.flatten()
+      |> Enum.reduce([], fn
+        {"fr."<>key, _value}, acc ->
+          acc ++ [key]
+        {"en."<>key, _value}, acc ->
+          acc ++ [key]
+        {key, _value}, acc ->
+          acc ++ [key]
+        end
+      )
+      |> Enum.uniq()
+    Agent.start_link(fn -> initial_value end, name: @name)
+  end
+
+  def delete_key(key) do
+    Agent.update(@name, fn list -> list -- [key] end)
+  end
+
+  def show_list() do
+    Agent.get(@name, & &1)
   end
 end
