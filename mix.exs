fb2f3adb5cd0d3200be978d497bf440414001276
Lukasz D <lukasz.dacz@llinformatics.com>
2022-09-01 10:01:57 +0200

    correct mix

--- mix.exs
+++ mix.exs
@@ -14,7 +14,8 @@ defmodule Checkeyatex.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {Checkeyatex, []}
     ]
   end
 
